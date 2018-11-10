814ea51ae6a0c8d8ff4e661bf18f350460fc2323
Johan Alm <jam@learningwell.se>
2018-11-10 01:15:58 +0100

    initial commit client

---
+++ client/src/server.js
@@ -0,0 +1,30 @@
+import sirv from 'sirv';
+import polka from 'polka';
+import compression from 'compression';
+import * as sapper from '../__sapper__/server.js';
+import { Store } from 'svelte/store.js';
+
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+
+const user = {
+	name: "Johan",
+	email: "jam@learningwell.se"
+};
+
+polka() // You can also use Express
+	.use(
+		compression({ threshold: 0 }),
+		sirv('static', { dev }),
+		sapper.middleware({
+      store: () => {
+        return new Store({
+					loggedIn: false,
+          user
+        });
+      }
+    })
+	)
+	.listen(PORT, err => {
+		if (err) console.log('error', err);
+	});
